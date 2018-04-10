49f978d7d22781bbf08bf01c94d957926f11ee74
joedundas <jdunda1@gmail.com>
2018-04-10 10:11:04 -0700

    Add user to company role of owner when creates new company

--- app/controllers/CompaniesController.php
+++ app/controllers/CompaniesController.php
@@ -22,7 +22,16 @@ class CompaniesController extends BaseController {
         $company->save();
 
         if($company->id > 0) {
+
+            $companyRole = new CompanyRoles();
+            $companyRole->companyId = $company->id;
+            $companyRole->userId = Auth::user()->id;
+            $companyRole->roleId = 1;
+            $companyRole->save();
+
+            if($companyRole->id > 0) {
                 return array('output' => array('errors' => 'none'));
+            }
         }
 
         return array('output'=>array('errors'=>'internal','data'=>array('We had internal issues when trying to create your company')));

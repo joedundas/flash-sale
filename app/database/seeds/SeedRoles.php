5393b7699abdfdd4fab2ed24a31623130218e99c
joedundas <jdunda1@gmail.com>
2018-04-10 09:55:47 -0700

    Seeding of roles

---
+++ app/database/seeds/SeedRoles.php
@@ -0,0 +1,21 @@
+<?php
+
+class SeedUsers extends Seeder
+{
+    public function run() {
+
+        DB::table('roles')->delete();
+
+        DB::table('roles')->insert(
+            array(
+                array('id'=>1,'title'=>'Owner', 'description'=>'Owner of the account'),
+                array('id'=>2,'title'=>'Admin','description'=>'Administrator of the account'),
+            )
+        );
+
+    }
+
+}
+
+
+
